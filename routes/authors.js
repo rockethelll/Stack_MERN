221ef71386ca01e1453bbf8e17780aa05f38b20a
rockethelll <51137055+rockethelll@users.noreply.github.com>
2023-08-12 18:45:47 +0200

    add author routes and views

---
+++ routes/authors.js
@@ -0,0 +1,45 @@
+import express from "express";
+import Author from "../models/author.js";
+
+const router = express.Router();
+
+// All Authors route
+router.get("/", async (req, res) => {
+  let searchOptions = {};
+  if (req.query.name !== null && req.query.name !== '') {
+  searchOptions.name = new RegExp(req.query.name, 'i');
+  }
+  try {
+    const authors = await Author.find(searchOptions)
+    res.render('authors/index', {
+      authors: authors,
+      searchOptions: req.query
+    });
+  } catch {
+    res.redirect('/');
+  }
+});
+
+// New Author route
+router.get('/new', (req, res) => {
+  res.render('authors/new', { author: new Author() });
+})
+
+// Create Author route
+router.post('/', async (req, res) => {
+  const author = new Author({
+    name: req.body.name
+  })
+  try {
+    const newAuthor = await author.save();
+    // res.redirect(`authors/${newAuthor.id}`);
+    res.redirect(`authors`);
+  } catch {
+    res.render('authors/new', {
+      author: author,
+      errorMessage: 'Error creating Author'
+    });
+  }
+});
+
+export default router;
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import expressLayouts from 'express-ejs-layouts';
-import indexRouter from './routes/index.js';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import indexRouter from './routes/index.js';
+import authorRouter from './routes/authors.js';
 
 if (process.env.NODE_ENV !== 'production') {
   dotenv.config();
@@ -15,6 +17,7 @@ app.set('views', './views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 mongoose
   .connect(process.env.DATABASE_URL, {
@@ -25,6 +28,7 @@ mongoose
     console.log('Server is running on port 3000');
   });
   app.use('/', indexRouter);
+  app.use('/authors', authorRouter);
 })
   .catch(err => {
     console.log(`DB Connection Error: ${err.message}`);
